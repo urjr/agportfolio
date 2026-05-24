821e2c8cb7d8abf20c705d5cdb9836c792983efd
Ulises Reyes <urjr@gmail.com>
2026-05-24 09:38:10 -0400

    feat: replace homepage text with biography introduction and add non-wrapping keyword links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="main-content">
-      <h1 className="hero-title" id="hero-title">
-        Hello World
+      <h1 className="hero-title intro-title" id="hero-title">
+        Hi, my name is{" "}
+        <Link href="#" className="nowrap-link" id="link-ulises">
+          Ulises Reyes-Kaura
+        </Link>
+        , I am a product designer and professor based in{" "}
+        <Link href="#" id="link-philadelphia">
+          Philadelphia
+        </Link>
+        . Currently, I work at{" "}
+        <Link href="#" id="link-google">
+          Google
+        </Link>
+        , designing agentic experiences for{" "}
+        <Link href="#" className="nowrap-link" id="link-google-analytics">
+          Google Analytics
+        </Link>
+        , and teach design at{" "}
+        <Link href="#" id="link-upenn">
+          UPenn
+        </Link>
       </h1>
     </main>
   );
