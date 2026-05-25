27cd5bd8496ff456e85c359a0c4b40fd35142b82
Ulises Reyes <urjr@gmail.com>
2026-05-24 21:44:35 -0400

    feat: split biography statement into stacked block items, scaling down the second block size by half

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,37 +3,42 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="main-content">
-      <h1 className="hero-title intro-title" id="hero-title">
-        Hi, my name is{" "}
-        <Link href="#" className="nowrap-link" id="link-ulises">
-          Ulises Reyes-Kaura
-        </Link>
-        . I am a{" "}
-        <Link href="#" className="nowrap-link" id="link-product-designer">
-          product designer
-        </Link>{" "}
-        and{" "}
-        <Link href="#" id="link-educator">
-          educator
-        </Link>{" "}
-        based in{" "}
-        <Link href="#" id="link-philadelphia">
-          Philadelphia
-        </Link>
-        . Currently, I work at{" "}
-        <Link href="#" id="link-google">
-          Google
-        </Link>
-        , designing agentic experiences for{" "}
-        <Link href="#" className="nowrap-link" id="link-google-analytics">
-          Google Analytics
-        </Link>
-        , and teach design at{" "}
-        <Link href="#" id="link-upenn">
-          UPenn
-        </Link>
-        .
-      </h1>
+      <div className="bio-container">
+        <h1 className="hero-title intro-title first-line" id="hero-title">
+          Hi, my name is{" "}
+          <Link href="#" className="nowrap-link" id="link-ulises">
+            Ulises Reyes-Kaura
+          </Link>
+          . I am a{" "}
+          <Link href="#" className="nowrap-link" id="link-product-designer">
+            product designer
+          </Link>{" "}
+          and{" "}
+          <Link href="#" id="link-educator">
+            educator
+          </Link>{" "}
+          based in{" "}
+          <Link href="#" id="link-philadelphia">
+            Philadelphia
+          </Link>
+          .
+        </h1>
+        <p className="hero-title intro-title second-line" id="hero-subtitle">
+          Currently, I work at{" "}
+          <Link href="#" id="link-google">
+            Google
+          </Link>
+          , designing agentic experiences for{" "}
+          <Link href="#" className="nowrap-link" id="link-google-analytics">
+            Google Analytics
+          </Link>
+          , and teach design at{" "}
+          <Link href="#" id="link-upenn">
+            UPenn
+          </Link>
+          .
+        </p>
+      </div>
     </main>
   );
 }
